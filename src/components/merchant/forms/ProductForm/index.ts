b2c23ac91775dfd054df77bcd58c218233816761
Arik Katz <arik@Ariks-MacBook-Air-2.local>
2025-05-05 09:43:16 +0300

    Fix ProductForm export in index.ts to resolve build error

--- src/components/merchant/forms/ProductForm/index.ts
+++ src/components/merchant/forms/ProductForm/index.ts
@@ -1,4 +1,5 @@
 // Export components
+export { ProductForm } from './index.tsx';
 export { ProductBasicInfo } from './ProductBasicInfo';
 export { CategorySelect } from './CategorySelect';
 export { PricingCurveEditor } from './PricingCurveEditor'; 
