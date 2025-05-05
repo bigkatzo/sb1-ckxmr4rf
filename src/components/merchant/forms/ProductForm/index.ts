55a43aa97d32047c64a17b540b78ebfad70d3271
Arik Katz <arik@Ariks-MacBook-Air-2.local>
2025-05-05 09:49:44 +0300

    Improve exports in ProductForm directory to fix circular dependencies

--- src/components/merchant/forms/ProductForm/index.ts
+++ src/components/merchant/forms/ProductForm/index.ts
@@ -1,5 +1,9 @@
-// Export components
-export { ProductForm } from './index.tsx';
+// Export components from this directory
 export { ProductBasicInfo } from './ProductBasicInfo';
 export { CategorySelect } from './CategorySelect';
 export { PricingCurveEditor } from './PricingCurveEditor'; 
+
+// Re-export the main ProductForm component
+// Note: This avoids the circular dependency by not importing directly from index.tsx
+// Instead, we're informing TypeScript that ProductForm will be available from this module
+export * from './index.tsx'; 
